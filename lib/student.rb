20f0b343e95a4183fd933b1213af75de743b62b7
Ryan Pethel <arpethel@gmail.com>
2019-04-13 23:56:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  def initialize
+    KNOWLEDGE = []
+  end
 
+  def learn(knowledge)
+    KNOWLEDGE << knowledge
+  end
+
+  def knowledge
+    self.KNOWLEDGE
+  end
+
+  
 end
