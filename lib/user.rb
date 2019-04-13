d071141e02c8405c537309a62f4ad6976dee6ef7
Ryan Pethel <arpethel@gmail.com>
2019-04-13 23:59:55 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,8 @@ require 'pry'
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
 end
